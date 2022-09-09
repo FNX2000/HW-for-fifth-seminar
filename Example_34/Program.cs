5a677c5fa87cf4f12f5140670b2b5165f8d53189
Vyacheslav Skapishev <slavappscab@mail.ru>
2022-09-09 19:33:01 +0300

    Упражнение 34

---
+++ Example_34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+ //Напишите программу, которая покажет количество чётных чисел в массиве.
+
+ void FillArray(int[]MyArray)
+{
+    for(int i=0; i < MyArray.Length; i++)
+    {
+        MyArray[i]=new Random().Next(100,1000);
+    }
+}
+void PrintArr(int[] MyArray)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", MyArray));
+}
+int QuantityEvenNumber(int[] myArray)
+{
+    int count = 0;
+    for(int i =0; i < myArray.Length; i++)
+    {
+        if(myArray[i] %2 ==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+//GETTINGCODE//
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] MyArray = new int[size];
+FillArray(MyArray);
+PrintArr(MyArray);//оставил для наглядного примера и проверки.
+Console.WriteLine($"Количество четных чисел равно {QuantityEvenNumber(MyArray)} ");
