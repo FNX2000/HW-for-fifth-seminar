f06648551ce3692e5eb168243b0e6a264f4fc787
Vyacheslav Skapishev <slavappscab@mail.ru>
2022-09-09 19:56:30 +0300

    Упражнение 36

---
+++ Example_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях (под позицией подразумевается индекс элемента).
+
+void FillArray(int[]MyArray)
+{
+    for(int i=0; i < MyArray.Length; i++)
+    {
+        MyArray[i]=new Random().Next(-100,101);
+    }
+}
+void PrintArr(int[] MyArray)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", MyArray));
+}
+int SumOddNumbers(int[] MyArray)
+{
+    int sum =0;
+    for(int i = 1; i< MyArray.Length; i++)
+    {
+        sum+=MyArray[i++];
+    }
+    return sum;
+}
+//GETTINGCODE//
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[size];
+FillArray(myArray);
+PrintArr(myArray);
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях равно {SumOddNumbers(myArray)} ");
